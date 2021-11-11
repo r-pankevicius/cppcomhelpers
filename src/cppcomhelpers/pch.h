5e3279ed598d029239701ec179f28e926c3e33e1
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2021-11-11 11:14:12 +0200

    Create Google test playground

---
+++ src/cppcomhelpers/pch.h
@@ -0,0 +1,12 @@
+// pch.h: This is a precompiled header file.
+#ifndef PCH_H
+#define PCH_H
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+
+// add headers that you want to pre-compile here
+
+
+
+#endif //PCH_H
---
+++ src/cppcomhelpersTests/cppcomhelpers_test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "cppcomhelpers.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
