56f689a53ea1970d5451370c0401a7325f5175c6
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2021-11-12 20:42:52 +0200

    Get and Put methods

--- src/cppcomhelpers/cppcomhelpers.h
+++ src/cppcomhelpers/cppcomhelpers.h
@@ -1,3 +1,39 @@
+// cppcomhelpers.h
+
 #pragma once
 
+#include <atlbase.h>
+#include <atlcom.h>
+#include <wtypes.h>
+#include <vcruntime.h>
+#include <winerror.h>
 
+/// <summary>
+/// Utility functions to make standard COM methods implementation (STDMETHODIMP) easier.
+/// </summary>
+class cppcomhelpers
+{
+public:
+	/// <summary>
+	/// Helper function to implement get_PropertyName(T* pRetVal): *pRetVal = value.
+	/// Use when no type conversions are needed between the backing value and return value.
+	/// </summary>
+	template<class T> static HRESULT Get(const T value, T* pRetVal) noexcept
+	{
+		if (NULL == pRetVal)
+			return E_POINTER;
+
+		*pRetVal = value;
+		return S_OK;
+	}
+
+	/// <summary>
+	/// Helper function to implement put_PropertyName(T newValue): refValueToSet = newValue.
+	/// Use when no type conversions are needed between value to be assigned (newValue) and the backing value.
+	/// </summary>
+	template<class T> static HRESULT Put(const T newValue, T& refValueToSet) noexcept
+	{
+		refValueToSet = newValue;
+		return S_OK;
+	}
+};
